cbabb79fcaa2423cb717a5221336d482b38f5609
Jason Burkybile <jburky@gmail.com>
2020-07-14 19:44:34 -0400

    Auto deploy

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "82a80d5bf0292897c2ec53c6591a05c6",
+  "main.dart.js": "2ae7b68317bfbe79d3845d2b0ad32bb6",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
